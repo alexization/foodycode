25728ce7e5af4f8c710473e537d7fc0313b7402d
codeforchoi <codeforchoi@gmail.com>
2023-07-27 00:48:26 +0900

    feat: 컴포넌트 추가 및 gitignore 설정

---
+++ frontend/src/components/MenuCard.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="component-6">
+        <div class="rectangle-267"></div>
+
+        <div class="rectangle-179"></div>
+
+        <div class="rich-soybean-paste-jjigae">Rich Soybean Paste Jjigae</div>
+
+        <div class="rich-soybean-paste-soup-braised-with-dried-anchovy-zucchini-onion-potato-garlic-mushroom-tofu-green-onion">
+            Rich Soybean Paste Soup braised with dried anchovy, zucchini, onion, potato,
+            garlic, mushroom, tofu, green onion<br />
+        </div>
+    </div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.component-6,
+.component-6 * {
+  box-sizing: border-box;
+}
+.component-6 {
+  width: 390px;
+  height: 160px;
+  position: relative;
+}
+.rectangle-267 {
+  border-style: solid;
+  border-color: #2afb73;
+  border-width: 1px;
+  width: 390px;
+  height: 160px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+}
+.rectangle-179 {
+  background: rgba(0, 0, 0, 0.3);
+  width: 128px;
+  height: 86px;
+  position: absolute;
+  left: 252px;
+  top: 10px;
+}
+.rich-soybean-paste-jjigae {
+  color: #000000;
+  text-align: left;
+  font: 600 15px "Noto Sans", sans-serif;
+  position: absolute;
+  left: 20px;
+  top: 20px;
+  width: 222px;
+  height: 22px;
+}
+.rich-soybean-paste-soup-braised-with-dried-anchovy-zucchini-onion-potato-garlic-mushroom-tofu-green-onion {
+  color: #000000;
+  text-align: left;
+  font: 300 12px "Noto Sans", sans-serif;
+  position: absolute;
+  left: 20px;
+  top: 102px;
+  width: 350px;
+  height: 49px;
+}
+
+</style>
