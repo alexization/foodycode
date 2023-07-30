4ef7046daaf22cec565dbe854c29a6c7e77fb234
LeeSeungUk <tmddnr5410@gmail.com>
2023-07-31 00:19:43 +0900

    feat : backend 폴더정리

--- backend/app/app.js
+++ backend/app/app.js
@@ -4,9 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var loginRouter = require("./routes/login");
+var indexRouter = require("./src/routes/index");
+var usersRouter = require("./src/routes/users");
+var loginRouter = require("./src/routes/login");
 
 var app = express();
 
--- backend/app/src/routes/login.js
+++ backend/app/src/routes/login.js
@@ -11,4 +11,10 @@ router.get("/", function (req, res, next) {
 router.post("/", UserCtrl.processLogin);
 
 
+// //register경로로 포스트요청이 오면 실행
+// router.post("/register",UserCtrl.processRegister);
+
+
 module.exports = router;
+
+//여기에 users관련된거를 다 넣어야할듯 파일이름도 바꾸고 그리고 로그인 라우터 따로 만들어야할듯
