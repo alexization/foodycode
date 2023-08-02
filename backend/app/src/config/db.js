703e196522a18c76c8d53502e71eac0c3ba367d1
LeeSeungUk <tmddnr5410@gmail.com>
2023-08-03 02:00:42 +0900

    feat : 회원가입 수정본 백엔드연결

--- backend/app/src/config/db.js
+++ backend/app/src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     host:"localhost",  
     user: "root", // mysql에 아이디를 넣는다.
-    password: "back0106", // mysql의 비밀번호를 넣는다.
+    password: "work", // mysql의 비밀번호를 넣는다.
     database: "frdg", //위에서 만든 데이터베이스의 이름을 넣는다.
 
 });
