7097f027cb844e4636ea55d4b537e442a4042090
HyoSeok Baek <alexization@gmail.com>
2023-08-05 02:20:00 +0900

    SignUp3View 기능 추가

--- frontend/src/views/SignUp2View.vue
+++ frontend/src/views/SignUp2View.vue
@@ -73,10 +73,10 @@
 </template>
 
 <script>
-import arrow_back from "@/assets/icon/arrow-back.png";
-import line from "@/assets/icon/Line.png";
+import arrow_back from '@/assets/icon/arrow-back.png';
+import line from '@/assets/icon/Line.png';
 
-import axios from "axios";
+import axios from 'axios';
 export default {
   data() {
     return {
@@ -86,27 +86,27 @@ export default {
   },
   methods: {
     click_back() {
-      location.href = "#/login";
+      location.href = '#/login';
     },
     click_confirm() {
-      alert("Click Confirm");
+      alert('Click Confirm');
     },
     click_male() {
-      document.getElementById("male").style.backgroundColor = "#1c9181";
-      document.getElementById("female").style.backgroundColor = "white";
+      document.getElementById('male').style.backgroundColor = '#1c9181';
+      document.getElementById('female').style.backgroundColor = 'white';
     },
     click_female() {
-      document.getElementById("female").style.backgroundColor = "#1c9181";
-      document.getElementById("male").style.backgroundColor = "white";
+      document.getElementById('female').style.backgroundColor = '#1c9181';
+      document.getElementById('male').style.backgroundColor = 'white';
     },
     async click_register() {
-      const uid = document.getElementById("uid");
+      const uid = document.getElementById('uid');
       console.log(uid.value);
-      const password = document.getElementById("pw");
+      const password = document.getElementById('pw');
       console.log(pw.value);
       let res = await axios({
-        method: "POST",
-        url: "api/signup",
+        method: 'POST',
+        url: 'api/signup',
         data: {
           uid: uid.value,
           password: password.value,
@@ -115,8 +115,8 @@ export default {
         console.log(res.data);
 
         if (res.data.success) {
-          alert("회원가입 완료! 로그인 페이지로 이동합니다.");
-          location.href = "#/login";
+          alert('회원가입 완료! 로그인 페이지로 이동합니다.');
+          location.href = '#/login';
         } else {
           alert(res.data);
         }
@@ -127,8 +127,8 @@ export default {
 </script>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Dangrek&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@500;600;700;800&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Dangrek&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@500;600;700;800&display=swap');
 
 .sign-up-page-register,
 .sign-up-page-register * {
@@ -150,6 +150,7 @@ export default {
   overflow: visible;
   border: none;
   background-color: white;
+  cursor: pointer;
 }
 .group-121 {
   position: sticky;
@@ -167,7 +168,7 @@ export default {
   top: 139px;
   color: #000000;
   text-align: center;
-  font: 600 15px "Noto Sans", sans-serif;
+  font: 600 15px 'Noto Sans', sans-serif;
   padding-top: 5px;
 }
 
@@ -185,7 +186,7 @@ export default {
   top: 139px;
   color: #ffffff;
   text-align: center;
-  font: 600 15px "Noto Sans", sans-serif;
+  font: 600 15px 'Noto Sans', sans-serif;
   padding-top: 5px;
 }
 
@@ -205,7 +206,7 @@ export default {
   top: 139px;
   color: #000000;
   text-align: center;
-  font: 600 15px "Noto Sans", sans-serif;
+  font: 600 15px 'Noto Sans', sans-serif;
   padding-top: 5px;
 }
 
@@ -224,27 +225,27 @@ export default {
 .uid {
   color: #000000;
   text-align: left;
-  font: 400 17px "Noto Sans", sans-serif;
+  font: 400 17px 'Noto Sans', sans-serif;
   position: absolute;
   left: 30px;
   top: 219px;
 }
 
-input[type="text"] {
+input[type='text'] {
   border-style: solid;
   border-color: #1c9181;
   border-width: 1px;
   width: 257px;
   height: 30px;
 }
-input[type][id="user_name"] {
+input[type][id='user_name'] {
   border-style: solid;
   border-color: #1c9181;
   border-width: 1px;
   width: 330px;
   height: 30px;
 }
-input[type][id="country"] {
+input[type][id='country'] {
   border-style: solid;
   border-color: #1c9181;
   border-width: 1px;
@@ -252,14 +253,14 @@ input[type][id="country"] {
   height: 30px;
 }
 
-input[type="password"] {
+input[type='password'] {
   border-style: solid;
   border-color: #1c9181;
   border-width: 1px;
   width: 330px;
   height: 30px;
 }
-input[type="number"] {
+input[type='number'] {
   border-style: solid;
   border-color: #1c9181;
   border-width: 1px;
@@ -282,14 +283,15 @@ input[type="number"] {
   left: 285px;
   top: 252px;
   text-align: center;
-  font: 600 14px "Noto Sans", sans-serif;
+  font: 600 14px 'Noto Sans', sans-serif;
   color: black;
+  cursor: pointer;
 }
 
 .password {
   color: #000000;
   text-align: left;
-  font: 400 17px "Noto Sans", sans-serif;
+  font: 400 17px 'Noto Sans', sans-serif;
   position: absolute;
   left: 30px;
   top: 322px;
@@ -302,7 +304,7 @@ input[type="number"] {
 .password-confrim {
   color: #000000;
   text-align: left;
-  font: 400 17px "Noto Sans", sans-serif;
+  font: 400 17px 'Noto Sans', sans-serif;
   position: absolute;
   left: 30px;
   top: 400px;
@@ -315,7 +317,7 @@ input[type="number"] {
 .user-name {
   color: #000000;
   text-align: left;
-  font: 400 17px "Noto Sans", sans-serif;
+  font: 400 17px 'Noto Sans', sans-serif;
   position: absolute;
   left: 30px;
   top: 503px;
@@ -328,7 +330,7 @@ input[type="number"] {
 .gender {
   color: #000000;
   text-align: left;
-  font: 400 17px "Noto Sans", sans-serif;
+  font: 400 17px 'Noto Sans', sans-serif;
   position: absolute;
   left: 30px;
   top: 659px;
@@ -345,7 +347,9 @@ input[type="number"] {
   left: 195px;
   top: 692px;
   text-align: center;
-  font: 400 14px "Noto Sans", sans-serif;
+  font: 400 14px 'Noto Sans', sans-serif;
+  cursor: pointer;
+  transition: 0.5s;
 }
 .Male {
   color: black;
@@ -359,12 +363,14 @@ input[type="number"] {
   left: 30px;
   top: 692px;
   text-align: center;
-  font: 400 14px "Noto Sans", sans-serif;
+  font: 400 14px 'Noto Sans', sans-serif;
+  cursor: pointer;
+  transition: 0.5s;
 }
 .Age {
   color: #000000;
   text-align: left;
-  font: 400 17px "Noto Sans", sans-serif;
+  font: 400 17px 'Noto Sans', sans-serif;
   position: absolute;
   left: 30px;
   top: 581px;
@@ -372,7 +378,7 @@ input[type="number"] {
 .Country {
   color: #000000;
   text-align: left;
-  font: 400 17px "Noto Sans", sans-serif;
+  font: 400 17px 'Noto Sans', sans-serif;
   position: absolute;
   left: 150px;
   top: 581px;
@@ -402,13 +408,15 @@ input[type="number"] {
   top: 784px;
   color: #ffffff;
   text-align: center;
-  font: 400 20px "Noto Sans", sans-serif;
+  font: 800 20px 'Noto Sans', sans-serif;
+  letter-spacing: 1.2px;
   border: none;
+  cursor: pointer;
 }
 .foody-logo {
   color: #1c9181;
   text-align: left;
-  font-family: "Dangrek", cursive;
+  font-family: 'Dangrek', cursive;
   font-size: 34px;
   font-style: normal;
   font-weight: 400;
--- frontend/src/views/SignUp3View.vue
+++ frontend/src/views/SignUp3View.vue
@@ -1,35 +1,159 @@
 <template>
   <div class="selecting-page">
     <div class="button-frame">
-      <div class="frame-row">
-        <div class="group-row">
-          <input
-            type="checkbox"
-            class="rectangle-first"
-            v-model="allergy"
-            value="eggs"
-          />
-          Eggs
+      <div class="frame-button">
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="abalone" />
+            <span>Abalone</span>
+          </label>
         </div>
-
-        <div class="group-row">
-          <input
-            type="checkbox"
-            class="rectangle-second"
-            v-model="allergy"
-            value="milk"
-          />
-          Milk
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="chicken" />
+            <span>Chicken</span>
+          </label>
         </div>
-
-        <div class="group-row">
-          <input
-            type="checkbox"
-            class="rectangle-third"
-            v-model="allergy"
-            value="peanuts"
-          />
-          Peanuts
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="mackerel" />
+            <span>Mackerel</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="oyster" />
+            <span>Oyster</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="pork" />
+            <span>Pork</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="soy" />
+            <span>Soy</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="walnut" />
+            <span>Walnut</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="none" />
+            <span>None</span>
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="button-frame-second">
+      <div class="frame-button">
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="beef" />
+            <span>Beef</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="crab" />
+            <span>Crab</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="milk" />
+            <span>Milk</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="peach" />
+            <span>Peach</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="shellfish" />
+            <span>Shellfish</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="sulfites" />
+            <span>Sulfites</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="wheat" />
+            <span>Wheat</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="none" />
+            <span>None</span>
+          </label>
+        </div>
+        <div></div>
+      </div>
+    </div>
+    <div class="button-frame-third">
+      <div class="frame-button">
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="buckwheat" />
+            <span>Buckwheat</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="egg" />
+            <span>Egg</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="mussel" />
+            <span>Mussel</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="peanut" />
+            <span>Peanut</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="shrimp" />
+            <span>Shrimp</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="tomato" />
+            <span>Tomato</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="none" />
+            <span>None</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" value="none" />
+            <span>None</span>
+          </label>
         </div>
       </div>
     </div>
@@ -77,8 +201,8 @@
 </template>
 
 <script>
-import arrow_back from "@/assets/icon/arrow-back.png";
-import line from "@/assets/icon/Line.png";
+import arrow_back from '@/assets/icon/arrow-back.png';
+import line from '@/assets/icon/Line.png';
 
 export default {
   data() {
@@ -90,19 +214,20 @@ export default {
   },
   methods: {
     click_back() {
-      location.href = "#/signup";
+      location.href = '#/signup';
     },
     click_next() {
       console.log(this.allergy);
+      alert(this.allergy);
     },
   },
 };
 </script>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Dangrek&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Jua&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Dangrek&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Jua&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800&display=swap');
 
 .selecting-page,
 .selecting-page * {
@@ -116,7 +241,7 @@ export default {
   overflow-y: auto;
 }
 .button-frame {
-  display: flex;
+  display: inline-flex;
   flex-direction: column;
   gap: 0px;
   align-items: flex-start;
@@ -125,63 +250,27 @@ export default {
   left: 30px;
   top: 336px;
 }
-.frame-row {
-  padding: 0px 0px 10px 0px;
-  display: flex;
-  flex-direction: row;
-  gap: 15px;
+.button-frame-second {
+  display: inline-flex;
+  flex-direction: column;
+  gap: 0px;
   align-items: flex-start;
   justify-content: flex-start;
-  flex-shrink: 0;
-  position: relative;
-}
-.group-row {
-  flex-shrink: 0;
-  width: 100px;
-  height: 42px;
-  position: static;
-}
-.rectangle-first {
-  background: #f1f1f1;
-  border-radius: 5px;
-  width: 100px;
-  height: 42px;
   position: absolute;
-  left: calc(50% - 165px);
-  top: calc(50% - 26px);
-  color: #000000;
-  text-align: center;
-  font: 400 17px "Noto Sans", sans-serif;
-  border: none;
+  left: 145px;
+  top: 336px;
+}
+.button-frame-third {
+  display: inline-flex;
+  flex-direction: column;
+  gap: 0px;
+  align-items: flex-start;
+  justify-content: flex-start;
+  position: absolute;
+  left: 260px;
+  top: 336px;
 }
 
-.rectangle-second {
-  background: #f1f1f1;
-  border-radius: 5px;
-  width: 100px;
-  height: 42px;
-  position: absolute;
-  left: calc(50% - 50px);
-  top: calc(50% - 26px);
-  color: #000000;
-  text-align: center;
-  font: 400 17px "Noto Sans", sans-serif;
-  border: none;
-}
-
-.rectangle-third {
-  background: #f1f1f1;
-  border-radius: 5px;
-  width: 100px;
-  height: 42px;
-  position: absolute;
-  left: calc(50% - -65px);
-  top: calc(50% - 26px);
-  color: #000000;
-  text-align: center;
-  font: 400 17px "Noto Sans", sans-serif;
-  border: none;
-}
 .arrow-back {
   position: absolute;
   left: 30px;
@@ -191,18 +280,21 @@ export default {
   overflow: visible;
   border: none;
   background-color: white;
+  cursor: pointer;
 }
 .next-button {
   background: #1c9181;
   width: 390px;
-  height: 45px;
+  height: 50px;
   position: absolute;
   left: 0px;
-  top: 1032px;
+  top: 794px;
   color: #ffffff;
   text-align: center;
-  font: 400 30px "Jua", sans-serif;
+  font: 400 30px 'Jua', sans-serif;
+  letter-spacing: 1.8px;
   border: none;
+  cursor: pointer;
 }
 
 .component-12 {
@@ -223,7 +315,7 @@ export default {
 .please-select-the-food-you-are-allergic-to-from-the-following {
   color: #000000;
   text-align: center;
-  font: 500 20px "Noto Sans", sans-serif;
+  font: 500 20px 'Noto Sans', sans-serif;
   position: absolute;
   left: 15.77px;
   top: 221.65px;
@@ -233,7 +325,7 @@ export default {
 .excludes-menus-containing-the-selected-food {
   color: #000000;
   text-align: center;
-  font: 300 13px "Noto Sans", sans-serif;
+  font: 300 13px 'Noto Sans', sans-serif;
   position: absolute;
   left: 35.74px;
   top: 283.6px;
@@ -256,7 +348,7 @@ export default {
   top: 139px;
   color: #000000;
   text-align: center;
-  font: 600 15px "Noto Sans", sans-serif;
+  font: 600 15px 'Noto Sans', sans-serif;
   padding-top: 5px;
 }
 
@@ -276,7 +368,7 @@ export default {
   top: 139px;
   color: #000000;
   text-align: center;
-  font: 600 15px "Noto Sans", sans-serif;
+  font: 600 15px 'Noto Sans', sans-serif;
   padding-top: 5px;
 }
 
@@ -297,7 +389,7 @@ export default {
   top: 139px;
   color: #ffffff;
   text-align: center;
-  font: 600 15px "Noto Sans", sans-serif;
+  font: 600 15px 'Noto Sans', sans-serif;
   padding-top: 5px;
 }
 
@@ -316,7 +408,7 @@ export default {
 .foody-logo {
   color: #1c9181;
   text-align: left;
-  font-family: "Dangrek", cursive;
+  font-family: 'Dangrek', cursive;
   font-size: 34px;
   font-style: normal;
   font-weight: 400;
@@ -328,7 +420,44 @@ export default {
   width: 102px;
   height: 33px;
 }
-input[type="checkbox"] {
+.frame-button {
+  max-width: 330px;
+  margin: 0 auto;
+  display: felx;
+  flex-wrap: wrap;
+}
+.frame-button div {
+  margin-bottom: 10px;
+}
+.frame-button div label {
+  cursor: pointer;
+}
+.frame-button div label input[type='checkbox'] {
   display: none;
+}
+.frame-button div label span {
+  position: relative;
+  display: inline-block;
+  background: #f1f1f1;
+  border-radius: 5px;
+  width: 100px;
+  height: 42px;
+  color: #000000;
+  text-align: center;
+  font: 400 17px 'Noto Sans', sans-serif;
+  padding-top: 9px;
+  transition: 0.5s;
+  overflow: hidden;
+}
+.frame-button div label span::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.frame-button div label input[type='checkbox']:checked ~ span {
+  background: #42b2a3;
 }
 </style>
