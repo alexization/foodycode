3d8f0f06c0c97036d0483f3f91d110b6545d22ec
codeforchoi <codeforchoi@gmail.com>
2023-08-08 01:54:42 +0900

    chore: dotenv setting

--- backend/app/app.js
+++ backend/app/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var dotenv = require("dotenv");
+dotenv.config();
 
 var indexRouter = require("./src/routes/index");
 var usersRouter = require("./src/routes/users");
