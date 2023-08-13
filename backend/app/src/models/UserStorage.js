200792d2ed1523921531d94fa3d5a87b68d38d71
LeeSeungUk <tmddnr5410@gmail.com>
2023-08-13 18:34:42 +0900

    feat : 회원가입 API 알러지 넣도록 추가

--- backend/app/src/models/UserStorage.js
+++ backend/app/src/models/UserStorage.js
@@ -28,26 +28,33 @@ class UserStorage{
 
     static async save(userInfo){
         return new Promise((resolve,reject) => {
-            // const query = "INSERT INTO users(uid,name,psword) VALUES(?,?,?);";
-            // db.query(query,[userInfo.uid,userInfo.name,userInfo.psword],
+            
             const info_query = "INSERT INTO users(uid,psword,name,age,gen,country,email) VALUES(?,?,'3','3','3','3','3');";
         
+            db.query(info_query,[userInfo.uid,userInfo.password],(err)=>{
+                console.log(info_query);
+                if(err) throw reject(`${err}`);
 
+                //resolve먼저 해버리면 알러지 전에 반환해버리니깐 나눠서
+                //resolve({success:true});
+            });
+
+            //inservalues에 빈문자열 할당, if문 후 쿼리 생성
             let insertValues = '';
+
             // 유저 알러지 정보가 하나라도 선택되어 있을때만 쿼리를 생성
             if (userInfo.arr_algid.length > 0) {
-                insertValues = arr_algid.map(algid => `(${algid}, (SELECT id FROM users WHERE uid = '${userInfo.uid}'))`).join(', ');
+                insertValues = userInfo.arr_algid.map(algid => `(${algid}, (SELECT id FROM users WHERE uid = '${userInfo.uid}'))`).join(', ');
+        
+                // insertValues가 생성될때만 alg_Query를 생성  (혹시모를 오류)
+                const alg_query = insertValues ? `INSERT INTO useralgs (algid, uid) VALUES    ${insertValues};` : '';
+
+                db.query(alg_query,[userInfo.uid,userInfo.password],(err)=>{
+                    if(err) throw reject(`${err}`);
+                    resolve({success:true});
+                });
+
             }
-
-            // insertValues가 생성될때만 alg_Query를 생성
-            const alg_Query = insertValues ? `INSERT INTO useralgs (algid, uid) VALUES    ${insertValues};` : '';
-
-            const query = `${info_query}\n${alg_Query}`;
-
-            db.query(query,[userInfo.uid,userInfo.password],(err)=>{
-                if(err) throw reject(`${err}`);
-                resolve({success:true});
-            });
         });
         
     }
