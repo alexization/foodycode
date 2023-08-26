6f9f70b8b3d9ea7fc2c0f946e79d9c0d88c194c6
HyoSeok Baek <alexization@gmail.com>
2023-08-26 19:06:13 +0900

    fix : v-for을 사용하기 위해 Allergy UI 재구성

--- frontend/src/components/Allergy.vue
+++ frontend/src/components/Allergy.vue
@@ -1,327 +1,5 @@
 <template>
   <div class="selecting-page">
-    <!-- <div class="button-frame">
-      <div class="frame-button">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="19" />
-            <span>Abalone</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="15" />
-            <span>Chicken</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="7" />
-            <span>Mackerel</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="20" />
-            <span>Oyster</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="10" />
-            <span>Pork</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="5" />
-            <span>Soy</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="14" />
-            <span>Walnut</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="none" />
-            <span>None</span>
-          </label>
-        </div>
-      </div>
-    </div>
-    <div class="button-frame-second">
-      <div class="frame-button">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="16" />
-            <span>Beef</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="8" />
-            <span>Crab</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="2" />
-            <span>Milk</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="11" />
-            <span>Peach</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="17" />
-            <span>Shellfish</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="13" />
-            <span>Sulfites</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="6" />
-            <span>Wheat</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="none" />
-            <span>None</span>
-          </label>
-        </div>
-        <div></div>
-      </div>
-    </div>
-    <div class="button-frame-third">
-      <div class="frame-button">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="3" />
-            <span>Buckwheat</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="1" />
-            <span>Egg</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="18" />
-            <span>Mussel</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="4" />
-            <span>Peanut</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="9" />
-            <span>Shrimp</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="none" />
-            <span>None</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="none" />
-            <span>None</span>
-          </label>
-        </div>
-      </div>
-    </div> -->
-
-    <div class="allergy-group">
-      <div class="allergy-row">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomatdfsdfasfo</span>
-          </label>
-        </div>
-      </div>
-      <div class="allergy-row">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-      </div>
-      <div class="allergy-row">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-      </div>
-      <div class="allergy-row">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-      </div>
-      <div class="allergy-row">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-      </div>
-      <div class="allergy-row">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-      </div>
-      <div class="allergy-row">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-      </div>
-      <div class="allergy-row">
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" v-model="allergy" value="12" />
-            <span>Tomato</span>
-          </label>
-        </div>
-      </div>
-    </div>
-    <button class="Register-button" @click="click_next">Register</button>
-
     <div class="please-select-the-food-you-are-allergic-to-from-the-following">
       Please select the food you are allergic to from the following.
     </div>
@@ -329,36 +7,59 @@
     <div class="excludes-menus-containing-the-selected-food">
       Excludes menus containing the selected food
     </div>
+
+    <div class="allergy-row">
+      <div v-for="item in allergy_list" :key="item">
+        <div>
+          <label>
+            <input type="checkbox" v-model="allergy" :value="item.number" />
+            <span v-text="item.name"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+
+    <div class="register">
+      <button class="Register-button" @click="click_next">Register</button>
+    </div>
   </div>
 </template>
 
 <script>
-import arrow_back from "@/assets/icon/arrow-back.png";
-import line from "@/assets/icon/Line.png";
+import arrow_back from '@/assets/icon/arrow-back.png';
+import line from '@/assets/icon/Line.png';
 
 export default {
   data() {
     return {
-      allergy: [],
+      allergy_list: [
+        { name: 'Abalone', number: '19' },
+        {
+          name: 'Beef',
+          number: '15',
+        },
+      ],
       arrow_back,
       line,
+      allergy: [],
     };
   },
   methods: {
     click_back() {
-      location.href = "#/signup";
+      location.href = '#/signup';
     },
     click_next() {
-      this.$emit("alg_data", this.allergy);
+      console.log(this.allergy_list);
+      this.$emit('alg_data', this.allergy);
     },
   },
 };
 </script>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Dangrek&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Jua&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Dangrek&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Jua&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800&display=swap');
 
 .selecting-page,
 .selecting-page * {
@@ -367,27 +68,30 @@ export default {
 .selecting-page {
   background: #ffffff;
   width: 100%;
-  height: 83.4vh;
-  top: 16.6vh;
+  height: calc(var(--vh, 1vh) * 100 - 130px);
+  top: 140px;
   position: absolute;
   overflow-y: auto;
 }
-.allergy-group {
+
+.allergy-row {
   top: 140px;
-  display: flex;
-  flex-direction: column;
   position: absolute;
   width: 100%;
-}
-.allergy-row {
-  margin-bottom: 10px;
   display: flex;
   justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+.allergy-row div {
+  display: flex;
+  width: 30%;
+  margin-bottom: 10px;
+  justify-content: center;
 }
 .allergy-row div label {
   cursor: pointer;
 }
-.allergy-row div label input[type="checkbox"] {
+.allergy-row div label input[type='checkbox'] {
   display: none;
 }
 .allergy-row div label span {
@@ -395,34 +99,37 @@ export default {
   display: inline-block;
   background: #f1f1f1;
   border-radius: 5px;
-  width: 100px;
+  width: 110px;
   height: 42px;
   color: #000000;
   text-align: center;
-  font: 400 17px "Noto Sans", sans-serif;
+  font: 400 17px 'Noto Sans', sans-serif;
   padding-top: 9px;
   transition: 0.5s;
   overflow: hidden;
 }
 .allergy-row div label span::before {
-  content: "";
+  content: '';
   position: absolute;
   top: 0;
   left: 0;
 }
-.allergy-row div label input[type="checkbox"]:checked ~ span {
+.allergy-row div label input[type='checkbox']:checked ~ span {
   background: #42b2a3;
+}
+.register {
+  position: absolute;
+  width: 100%;
+  height: 50px;
+  bottom: 0px;
 }
 .Register-button {
   background: #1c9181;
   width: 100%;
-  height: 49.8px;
-  position: absolute;
-  left: 0px;
-  top: 77.5vh;
+  height: 50px;
   color: #ffffff;
   text-align: center;
-  font: 400 30px "Jua", sans-serif;
+  font: 400 30px 'Jua', sans-serif;
   letter-spacing: 1.8px;
   border: none;
   cursor: pointer;
@@ -431,21 +138,21 @@ export default {
 .please-select-the-food-you-are-allergic-to-from-the-following {
   color: #000000;
   text-align: center;
-  font: 500 20px "Noto Sans", sans-serif;
+  font: 500 20px 'Noto Sans', sans-serif;
   position: absolute;
   left: 16px;
-  top: 2.4vh;
+  top: 20.3px;
   width: 91.8%;
-  height: 6.8vh;
+  height: 57.4px;
 }
 .excludes-menus-containing-the-selected-food {
   color: #000000;
   text-align: center;
-  font: 300 13px "Noto Sans", sans-serif;
+  font: 300 13px 'Noto Sans', sans-serif;
   position: absolute;
   left: 36px;
-  top: 9.5vh;
+  top: 81px;
   width: 81.3%;
-  height: 2.3vh;
+  height: 19.5px;
 }
 </style>
