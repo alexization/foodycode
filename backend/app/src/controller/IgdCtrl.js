48657c311cb7b12b2d601b2acffcc463caea6720
LeeSeungUk <tmddnr5410@gmail.com>
2023-09-06 19:49:30 +0900

    feat : 식재료 컨트롤러 구현:

--- backend/app/src/controller/IgdCtrl.js
+++ backend/app/src/controller/IgdCtrl.js
@@ -2,8 +2,13 @@ const IgdStorage = require("../models/IgdStroage");
 
 
 class IgdCtrl {
-    static async searchIgdNames(key){
-        
-       const response = await IgdStorage.getIgdNamesContain
+    static async searchIgdNames(req,res){
+
+       const key = req.params.menu_name;
+       const response = await IgdStorage.getIgdNamesContain(key);
+       
+       return res.send(response);
     }
 }
+
+module.exports = IgdCtrl;
