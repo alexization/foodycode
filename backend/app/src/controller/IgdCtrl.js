06f724e4dd5df4f0a3ff98b2a511cc22866a7bc7
hyoseok <alexization@gmail.com>
2023-09-06 21:54:53 +0900

    feat : 검색 버튼 눌렀을 때 데이터 GET

--- backend/app/src/controller/IgdCtrl.js
+++ backend/app/src/controller/IgdCtrl.js
@@ -1,14 +1,14 @@
 const IgdStorage = require("../models/IgdStroage");
 
-
 class IgdCtrl {
-    static async searchIgdNames(req,res){
+  static async searchIgdNames(req, res) {
+    console.log(req.params);
+    console.log(req.params.menu_name);
+    const key = req.params.menu_name;
+    const response = await IgdStorage.getIgdNamesContain(key);
 
-       const key = req.params.menu_name;
-       const response = await IgdStorage.getIgdNamesContain(key);
-       
-       return res.send(response);
-    }
+    return res.send(response);
+  }
 }
 
 module.exports = IgdCtrl;
--- frontend/src/views/AddMenu.vue
+++ frontend/src/views/AddMenu.vue
@@ -46,8 +46,8 @@
       <div class="ingd-group">
         <div class="ingd-search">
           <img src="@/assets/icon/search.png" />
-          <input type="text" />
-          <button @click="handleClick">검색</button>
+          <input type="text" id="ingd_name" />
+          <button @click="[handleClick(), get_alg()]">검색</button>
         </div>
       </div>
       <div class="allergy-text">
@@ -234,6 +234,13 @@ export default {
             alert("오류 발생");
           }
         });
+    },
+    get_alg() {
+      const ingdName = document.getElementById("ingd_name").value;
+      console.log(ingdName);
+      axios.get(`/api/igd/${ingdName}`).then((response) => {
+        console.log(response.data);
+      });
     },
   },
 };
