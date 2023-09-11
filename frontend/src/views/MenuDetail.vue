d5c3b2a9d28a991a799124b93d84a3e41fca6a3c
hyoseok <alexization@gmail.com>
2023-09-11 17:19:46 +0900

    fix : MenuDetail UI 수정

--- frontend/src/views/MenuDetail.vue
+++ frontend/src/views/MenuDetail.vue
@@ -1,54 +1,78 @@
 <template>
-  <div class="detail-page">
-    <div class="description-page">
-      <img class="menu-img" :src="require(`@/assets/menu/${img_url}.png`)" />
+  <div>
+    <div class="detail-header">
       <button class="arrow-back-background" @click="click_back">
         <img class="arrow-back" src="@/assets/icon/arrow_back_iOS.png" />
       </button>
-
-      <div class="menu-name" v-text="detail_list.menu_name"></div>
-      <div class="details">Details</div>
-      <div class="detail-text" v-text="detail_list.menu_ing"></div>
-      <div class="allergy-tag">Allergy</div>
-      <div class="price">Price</div>
-      <div class="menu-price" v-text="detail_list.menu_price + ' won'"></div>
-      <button class="remove-background" @click="click_remove">
-        <img class="remove" src="@/assets/icon/remove.png" />
-      </button>
-      <button class="add-background" @click="click_add">
-        <img class="add" src="@/assets/icon/add.png" />
-      </button>
-      <div class="count" v-text="menu_count"></div>
     </div>
-    <div class="Footer">
-      <button class="add-cart" @click="add_cart">Add to Cart</button>
+    <div class="detail-page">
+      <div class="description-page">
+        <img class="menu-img" :src="require(`@/assets/menu/${img_url}.png`)" />
+      </div>
+      <div class="description-group">
+        <div class="menu-name-group">
+          <div class="menu-name-eng" v-text="detail_list.menu_name"></div>
+          <div class="menu-name-rom">Default Roman Name</div>
+        </div>
+        <div class="detail-group">
+          <div class="details">Details</div>
+          <div class="detail-text" v-text="detail_list.menu_ing"></div>
+        </div>
+        <div class="allergy-group">
+          <div class="allergy">Allergy</div>
+          <div class="allergy-list"></div>
+        </div>
+      </div>
+    </div>
+    <div class="detail-footer">
+      <div class="price-group">
+        <div class="price-text">
+          <div
+            class="menu-price"
+            v-text="detail_list.menu_price + ' won'"
+          ></div>
+        </div>
+        <div class="price-button-group">
+          <div>
+            <button class="remove-background" @click="click_remove">
+              <img class="remove" src="@/assets/icon/remove.png" />
+            </button>
+          </div>
+          <div class="count" v-text="menu_count"></div>
+          <div>
+            <button class="add-background" @click="click_add">
+              <img class="add" src="@/assets/icon/add.png" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="button-group">
+        <button class="add-cart" @click="add_cart">Add to Cart</button>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import axios from 'axios';
-
-/* Code generated with AutoHTML Plugin for Figma */
+import axios from "axios";
 
 export default {
-  name: 'DescriptionPage',
+  name: "DescriptionPage",
   components: {},
   props: {},
   data() {
-    // quickfix to have components available to pass as props
     return {
-      prev_rest: '',
-      prev_url: '',
+      prev_rest: "",
+      prev_url: "",
       menu_count: 1,
       detail_list: [],
-      img_url: 'loading',
+      img_url: "loading",
       price: 0,
     };
   },
   methods: {
     click_remove() {
       if (this.menu_count < 2) {
-        alert('최소주문 개수입니다');
+        alert("최소주문 개수입니다");
       } else {
         this.menu_count -= 1;
         this.detail_list.menu_price =
@@ -61,11 +85,11 @@ export default {
         parseInt(this.detail_list.menu_price) + this.price;
     },
     click_back() {
-      this.prev_url = '#/allmenu/' + this.prev_rest;
+      this.prev_url = "#/allmenu/" + this.prev_rest;
       location.href = this.prev_url;
     },
     add_cart() {
-      alert('Comming Soon..');
+      alert("Comming Soon..");
     },
   },
   created() {
@@ -82,12 +106,32 @@ export default {
 };
 </script>
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@500;600;700;800&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@500;600;700;800&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap");
 
-.detail-page,
-.detail-page * {
-  box-sizing: border-box;
+.detail-header {
+  position: fixed;
+  z-index: 20;
+  width: 100%;
+  height: 100px;
+  max-width: 500px;
+}
+.arrow-back-background {
+  width: 60px;
+  height: 60px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.8);
+  border-radius: 30px;
+  border: none;
+  top: 25px;
+  left: 20px;
+  cursor: pointer;
+}
+.arrow-back-background img {
+  width: 32px;
+  height: 24px;
+  padding-top: 4px;
+  padding-left: 15px;
 }
 .detail-page {
   background: #ffffff;
@@ -97,93 +141,113 @@ export default {
 }
 .description-page {
   width: 100%;
-  height: calc(var(--hv, 1vh) * 100 - 50px);
+  height: 200px;
   position: absolute;
-  overflow-y: scroll;
+  top: 0px;
 }
 .menu-img {
   background: linear-gradient(to left, #d9d9d9, #d9d9d9);
   width: 100%;
-  height: 282px;
+  height: 100%;
+}
+.description-group {
+  width: 100%;
+  height: calc(var(--vh, 1vh) * 100 - 140px);
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  overflow-x: clip;
   position: absolute;
-  left: 0px;
-  top: 0px;
+  top: 200px;
 }
-
-.arrow-back-background {
-  width: 60px;
-  height: 60px;
-  position: absolute;
-  background: rgba(255, 255, 255, 0.8);
-  border-radius: 21px;
-  border: none;
-  top: 50px;
-  left: 20px;
-  cursor: pointer;
+.menu-name-group {
+  width: 100%;
+  height: 20%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  margin-left: 10px;
 }
-.arrow-back-background img {
-  width: 32px;
-  height: 24px;
-  padding-top: 3px;
-  padding-left: 8px;
-}
-
-.menu-name {
+.menu-name-eng {
   color: #000000;
-  text-align: left;
-  font: 500 16px 'Rubik', sans-serif;
-  position: absolute;
-  left: 5.1%;
-  top: 310px;
-  width: 88.5%;
+  font: 500 22px "Rubik", sans-serif;
+  width: 100%;
+}
+.menu-name-rom {
+  width: 100%;
+  font: 300 16px "Rubik", sans-serif;
+  color: #575757;
+}
+.detail-group {
+  width: 100%;
+  height: 30%;
+  margin-left: 10px;
 }
 .details {
-  color: #000000;
-  text-align: right;
-  font: 500 14px 'Rubik', sans-serif;
-  position: absolute;
-  left: 5.1%;
-  top: 362px;
+  width: 100%;
+  height: 20%;
+  color: black;
+  font: 500 18px "Rubik", sans-serif;
 }
 .detail-text {
-  color: linear-gradient(
-    180deg,
-    rgba(0, 0, 0, 1) 0%,
-    rgba(0, 0, 0, 1) 93.75%,
-    rgba(0, 0, 0, 0) 100%
-  );
-  text-align: left;
-  font: 400 10px 'Rubik', sans-serif;
-  position: absolute;
-  left: 5.1%;
-  top: 391px;
+  color: #575757;
+  font: 400 12px "Rubik", sans-serif;
   width: 90%;
   height: 101px;
 }
-.allergy-tag {
-  color: #000000;
-  text-align: right;
-  font: 500 14px 'Rubik', sans-serif;
-  position: absolute;
-  left: 5.1%;
-  top: 522px;
+.allergy-group {
+  width: 100%;
+  height: 30%;
+  margin-left: 10px;
 }
+.allergy {
+  color: #000000;
+  font: 500 18px "Rubik", sans-serif;
+}
+.detail-footer {
+  width: 100%;
+  height: 140px;
+  position: fixed;
+  background: none;
+  bottom: 0px;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  z-index: 20;
+}
+.price-group {
+  width: 100%;
+  height: 50%;
+  display: flex;
+  flex-direction: row;
+}
+.price-text {
+  width: 50%;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+}
+.price-button-group {
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.price-button-group div {
+  width: 30%;
+  display: flex;
+  justify-content: center;
+}
+
 .price {
   color: #000000;
   text-align: right;
-  font: 500 14px 'Rubik', sans-serif;
-  position: absolute;
-  left: 5.1%;
-  top: 662px;
+  font: 500 14px "Rubik", sans-serif;
 }
-
 .menu-price {
   color: #1c9181;
   text-align: left;
-  font: 400 19px 'Rubik', sans-serif;
-  position: absolute;
-  left: 5.1%;
-  top: 691px;
+  font: 400 19px "Rubik", sans-serif;
 }
 
 .remove-background {
@@ -191,9 +255,6 @@ export default {
   border-radius: 50%;
   width: 23px;
   height: 23px;
-  position: absolute;
-  left: 68.2%;
-  top: 693px;
   border: none;
   cursor: pointer;
 }
@@ -206,9 +267,6 @@ export default {
   border-radius: 50%;
   width: 23px;
   height: 23px;
-  position: absolute;
-  left: 88.2%;
-  top: 693px;
   border: none;
   cursor: pointer;
 }
@@ -219,27 +277,24 @@ export default {
 .count {
   color: #000000;
   text-align: left;
-  font: 400 18px 'Rubik', sans-serif;
-  position: absolute;
-  left: 80%;
-  top: 691px;
+  font: 400 18px "Rubik", sans-serif;
 }
-.Footer {
+.button-group {
   width: 100%;
-  height: 50px;
-  position: absolute;
-  bottom: 0px;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: white;
 }
 .add-cart {
   width: 90%;
   height: 40px;
-  position: absolute;
-  border-radius: 7px;
+  border-radius: 10px;
   background: #1c9181;
   border: none;
   color: white;
   margin-bottom: 10px;
-  left: 5%;
-  font: 400 18px 'Rubik', sans-serif;
+  font: 400 18px "Rubik", sans-serif;
 }
 </style>
