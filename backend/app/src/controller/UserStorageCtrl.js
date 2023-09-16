2773e1308d2cf39f7434c1a3cc92512f3c0e6c77
LeeSeungUk <tmddnr5410@gmail.com>
2023-09-17 02:45:06 +0900

    feat : 코드 가독성 증가

--- backend/app/src/controller/UserStorageCtrl.js
+++ backend/app/src/controller/UserStorageCtrl.js
@@ -2,8 +2,11 @@ const UserStorage = require('../models/UserStorage');
 
 class UserStorageCtrl {
   static async getUserlogined(req, res) {
-    if (!req.session.userid) return res.json({ is_logined: false });
-    else return res.json({ is_logined: true });
+    if (!req.session.userid) {
+      return res.json({ is_logined: false });
+    } else {
+      return res.json({ is_logined: true });
+    }
   }
 }
 module.exports = UserStorageCtrl;
