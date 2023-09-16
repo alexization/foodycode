0ee613dfac71f63fad6b9858386ba3f66faf31a3
LeeSeungUk <tmddnr5410@gmail.com>
2023-09-17 02:27:53 +0900

    feat : UserStorageCtorl 코드 수정

--- backend/app/src/controller/UserStorageCtrl.js
+++ backend/app/src/controller/UserStorageCtrl.js
@@ -3,7 +3,7 @@ const UserStorage = require('../models/UserStorage');
 class UserStorageCtrl {
   static async getUserInfo(req, res) {
     const uid = req.session.userid;
-    const response = await UserStorage.getUserInfo(uid);
+    const response = await UserStorage.getUsersInfo(uid);
     console.log(response);
     return res.json(response);
   }
